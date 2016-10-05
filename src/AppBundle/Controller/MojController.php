c8814487f397d170ae3bfb000e50fafed1dc03cd
Marek Ćwikliński <marek.cwiklinski@egm.pl>
2016-10-05 19:41:17 +0200

    dodany bower

--- src/AppBundle/Controller/MojController.php
+++ src/AppBundle/Controller/MojController.php
@@ -13,4 +13,25 @@ class MojController extends Controller
         // replace this example code with whatever you need
         return $this->render('AppBundle:Moj:index.html.twig');
     }
+
+    public function editAction()
+    {
+        return $this->render('@App/Moj/edit.html.twig');
+    }
+
+    public function getAction()
+    {
+        return $this->render('@App/Moj/get.html.twig');
+    }
+
+    public function addAction()
+    {
+        return $this->render('@App/Moj/add.html.twig');
+    }
+
+    public function deleteAction($id)
+    {
+        return $this->render('@App/Moj/delete.html.twig', ['id' => $id]);
+    }
+
 }
